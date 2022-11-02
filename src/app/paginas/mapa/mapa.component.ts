4a71b094ed18017947a5516e88a6a6036a7fca2f
jaider2609jr <jaider.rj11@gmail.com>
2022-11-01 22:57:23 -0500

    se agrego el mapa

--- src/app/paginas/mapa/mapa.component.ts
+++ src/app/paginas/mapa/mapa.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import "leaflet/dist/leaflet.css";
+import * as L from "leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  private map: any;
   constructor() { }
 
+
   ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map("map").setView([14.094167, -87.206667], 15);
+
+    const tiles = L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }
+    ).addTo(this.map);
+
+    tiles.addTo(this.map);
+
+    (L.Control as any).geocoder().addTo(this.map);
   }
 
 }
