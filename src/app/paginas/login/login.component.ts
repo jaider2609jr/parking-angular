90d8e6342f450cabadbf2b8b4b372c598b99741a
jaider2609jr <jaider.rj11@gmail.com>
2022-11-02 16:34:40 -0500

    se agrego formulario reactivo al login

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  loginUser!: FormGroup;
   constructor() { }
 
+  createForm(): void {
+    this.loginUser = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+
+  }
+
+
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  onSubmit(form:any): void {
+    console.log('username: ' + form.username);
+    console.log('password: ' + form.password);
   }
 
 }
